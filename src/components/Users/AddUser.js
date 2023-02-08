50886524e0f45f2dea5e7f46376bccccedd18c90
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-08 12:02:19 +0300

    Added an import for the Wrapper component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -4,22 +4,27 @@ import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css';
+import Wrapper from '../Helpers/Wrapper';
 
-const AddUser = (props) => {
+const AddUser = (props) =>
+{
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
   const [error, setError] = useState();
 
-  const addUserHandler = (event) => {
+  const addUserHandler = (event) =>
+  {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0)
+    {
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid name and age (non-empty values).',
       });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+enteredAge < 1)
+    {
       setError({
         title: 'Invalid age',
         message: 'Please enter a valid age (> 0).',
@@ -31,15 +36,18 @@ const AddUser = (props) => {
     setEnteredAge('');
   };
 
-  const usernameChangeHandler = (event) => {
+  const usernameChangeHandler = (event) =>
+  {
     setEnteredUsername(event.target.value);
   };
 
-  const ageChangeHandler = (event) => {
+  const ageChangeHandler = (event) =>
+  {
     setEnteredAge(event.target.value);
   };
 
-  const errorHandler = () => {
+  const errorHandler = () =>
+  {
     setError(null);
   };
 
